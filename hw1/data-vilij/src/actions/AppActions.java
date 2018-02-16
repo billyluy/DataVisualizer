baeec03b930e8dad8c84ca4298e39b5196bfaa9b
Kristy <kristy.tan@stonybrook.edu>
2018-02-16 00:55:45 -0500

    hw1 done

--- hw1/data-vilij/src/actions/AppActions.java
+++ hw1/data-vilij/src/actions/AppActions.java
@@ -105,7 +105,7 @@ public final class AppActions implements ActionComponent {
 
                 fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(manager.getPropertyValue(DATA_FILE_EXT.name()), manager.getPropertyValue(DATA_FILE_EXT_DESC.name())));
 
-                File file = fileChooser.showSaveDialog(applicationTemplate.getUIComponent().getPrimaryWindow()); ''
+                File file = fileChooser.showSaveDialog(applicationTemplate.getUIComponent().getPrimaryWindow()); 
                 if(file != null) {
                     FileWriter writing = new FileWriter(file);
                     writing.write(((AppUI) applicationTemplate.getUIComponent()).getTextArea());
